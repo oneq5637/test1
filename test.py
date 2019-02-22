edc4d7c5dc5247738b6b3f4d0e4658507560d655
oneq5637 <oneq5637@gmail.com>
2019-02-22 13:58:41 +0900

    comment

---
+++ test.py
@@ -0,0 +1,31 @@
+1 + 2
+
+3 / 2.4
+
+3 * 9
+
+a = 1
+b = 2
+a + b
+
+a  = "Python"
+print(a)
+
+a = 3
+if a > 1:
+    print("a is greater than 1")
+
+for a in [1, 2, 3]:
+    print(a)
+
+i = 0
+while i < 3:
+    i=i+1
+    print(i)
+
+
+def sum(a,b):
+    return a+b
+
+
+print(sum(3,4))
